cf90a44b5d84bf7021f8af8be1e24769bd9c9cae
Sergey Kolosov <kolosov@gmail.com>
2014-11-24 22:19:49 +0300

    md5 script update

--- scripts/script_3_md5.sh
+++ scripts/script_3_md5.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+ROOT_PATH=`pwd`
+MD5_SYS_DIR="${ROOT_PATH}/build"
+MD5_FILE='md5_log'
+
 echo "Calculate md5"
-find -type f  | xargs md5sum > md_log
+
+echo "MD5_SYS_DIR=$MD5_SYS_DIR"
+
+#find -type f  | xargs md5sum > md_log
+#find $ROOT_PATH -path $EXCLUDE_DIR -prune -o -type f -print | sort | xargs md5sum |gawk '{print $2,$1}' > $MD5_FILE
+echo "find $ROOT_PATH -path $MD5_SYS_DIR -prune -o -type f -print | xargs md5sum |gawk '{print $2,$1}'|sort > $MD5_FILE"
+find $ROOT_PATH -path $MD5_SYS_DIR -prune -o -type f -print | xargs md5sum |gawk '{print $2,$1}'|sort > $MD5_FILE
+
 echo "Done"
